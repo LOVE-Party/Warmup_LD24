ac403b0260590ed9e935d5f1355847c1ec7bdcd8
DMB <netsdanita@yahoo.com.au>
2012-08-23 03:39:05 +1000

    improved attack probability, and ability management

--- mon.lua
+++ mon.lua
@@ -28,46 +28,68 @@ function _M:new(t)
 	end
 	
 	-- Provisional stats, purely for the sake of testing.
-	m.speed    = t.speed or m.level -- dodge, initative
-	m.strength = t.power or m.level -- damage
-	m.tough    = t.tough or m.level -- Toughness, defence
+	m.speed     = t.speed     or m.level+2 -- dodge, initative
+	m.strength  = t.strength  or m.level+2 -- damage
+	m.tough     = t.tough     or m.level   -- Toughness, defence
 	m.healthmax = t.healthmax or t.health or (m.level * 7) + m.tough
-	m.health    = t.health or m.healthmax
+	m.health    = t.health    or m.healthmax
 	m.energymax = t.energymax or t.energy or (m.level * 3) + m.speed
-	m.energy    = t.energy or m.energymax
-	
+	m.energy    = t.energy    or m.energymax
 	
 	return m
 end
 
 function _M:attack(target, ability)
 	ability = ability or self.abilities[1]
-	local r = function(n) return math.random(n-1, n) end
-	local a, b = r(self.speed+ability.initative), r(target.speed)
-	local dam = 0
-	if a > b then
-		dam = ability:use(self, target)
+	if self:canuse(target, ability) then
+		local r = function(n) return math.random(10)+n end
+		local a, b = r(self.speed+ability.initative), target.speed+5
+		local dam = 0
+		if a > b then
+			dam = ability:use(self, target)
+		end
+		return a>b, a, b, dam
+	else
+		return false, 0, 0, 0
 	end
-	return a>b, a, b, dam
 end
 
-
-function _M:dobasicattack(target)
-	local r = function(n) return math.random(n-1, n) end
-	local a, b = r(self.speed), r(target.speed)
-	if a > b then
-		local dam  = self.strength - target.tough
-		dam = dam > 0 or 1
-		target:damage(dam)
-		return true, a, b, dam
+function _M:canuse(target, ability)
+	ability = ability or self.abilities[1]
+	if self.energy < ability.cost then
+		return false, 'expensive'
 	end
-	return false, a, b, 0
+	
+	return true, 'okay'
+end
+
+function _M:hasability(ability)
+	for k, v in pairs(self.abilities) do
+		if v == ability then return k end
+	end
+	return false
+end
+
+function _M:addability(ability)
+	assert(not self:hasability(ability), "Cannot add an ability twice")
+	self.abilities[#self.abilities+1] = ability
+	ability:aquire(self)
+end
+
+function _M:delability(ability)
+	for k, v in pairs(self.abilities) do
+		if v == ability then
+			table.remove(self.abilities, k)
+			ability:lose(self)
+			return v
+		end
+	end
 end
 
 function _M:damage(n)
 	assert(type(n) == 'number', "Damage must be a number")
 	assert(n >=0, "Damage must be a positive number")
-	n = n-self.tough
+	n = n-math.random(math.ceil(self.tough/2), self.tough)
 	n = n > 0 and n or 1
 	self.health = self.health - n
 	self.health = self.health >=0 and self.health or 0
